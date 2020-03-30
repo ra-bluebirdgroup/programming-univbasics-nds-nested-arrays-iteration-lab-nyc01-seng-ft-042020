8dc4663836e11ea2892a545161722c4f39bee155
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-30 03:24:07 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -45,8 +45,10 @@ while row_index < src.count do
   outer_results << inner_results
   row_index += 1
 end
-
-outer_results #=>
+if outer_results[0] > outer_results[1]
+   outer_results.pop
+ else
+   outer_results.unshift
 end
 
 def total_even_pairs(src)
