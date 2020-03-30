cee19a872e1b411d520a6568b0347943d1dd3773
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-30 04:34:05 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -89,9 +89,9 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index][0]%2==0 &&  src[row_index][1]%2==0 
+    if src[row_index][0]%2==0 &&  src[row_index][1]%2==0
     total += src[row_index][0] + src[row_index][1]
-    element_index += 1
+    element_index += 2
   end
   row_index += 1
 end
