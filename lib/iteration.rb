d84c5b07dbc8bf1119ecfd329082065d6aef7bb9
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-30 03:30:14 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -34,11 +34,11 @@ while row_index < src.count do
     #   Array at row_index
     #   Element of the inner array at element_index
     #   The first character of that element...
-    if src[row_index][element_index] > src[row_index][element_index]
+    if src[row_index][0] > src[row_index][1]
 
-      inner_results << src[row_index][element_index][0]
+      inner_results << src[row_index][element_index]
     else
-      inner_results << src[row_index][element_index][1]
+      inner_results << src[row_index][element_index]
     end
     element_index += 2
   end
