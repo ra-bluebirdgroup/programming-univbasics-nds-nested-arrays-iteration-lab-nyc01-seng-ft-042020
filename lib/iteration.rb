dcf2485b57cb8ce0a017ad68b11a19b33c2a5c83
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-30 04:53:13 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,7 +49,6 @@ while row_index < src.count do
  end
    outer_results.sort
    outer_results.flatten!
-   #outer_results.shift
    outer_results
 end
 
@@ -63,27 +62,6 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-#   sum = 0
-#   row_index = 0
-#
-#   while row_index < src.count do
-#
-#     element_index = 0
-#     inner_results = 0
-#
-#     while element_index < src[row_index].count do
-#
-#       if src[row_index][0].even? and src[row_index][1].even?
-#         sum += src[row_index][0] + src[row_index][1]
-#       end
-#
-#       element_index += 1
-#      end
-#
-#     row_index += 1
-#    end
-#
-#      sum
 total = 0
 row_index = 0
 
@@ -91,14 +69,13 @@ while row_index < src.count do
   element_index = 0
 
   while element_index < src[row_index].count do
+       if src[row_index][0].even? && src[row_index][1].even?
+       total += src[row_index][0] + src[row_index][1]
+       end
+      element_index += 2
+   end
 
-    if src[row_index][0].even? && src[row_index][1].even?
-    total += src[row_index][0] + src[row_index][1]
-    end
-    element_index += 2
-  end
-row_index += 1 
-
+ row_index += 1
 end
 
 total
